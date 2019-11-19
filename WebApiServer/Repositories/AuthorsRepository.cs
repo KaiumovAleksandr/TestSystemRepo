6a3596c43d7ce64ccb3086af1efa46ad62e2a96f
KaiumovAleksandr <kayumovalexandr77@gmail.com>
2019-11-19 10:41:08 +0200

    add author-contoroller

---
+++ WebApiServer/Repositories/AuthorsRepository.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Common;
+
+namespace WebApiServer.Repositories
+{
+    public class AuthorsRepository:IAuthorsRepository
+    {
+        public IEnumerable<Author> Get()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Author Get(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int Create(Author author)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Author Update(Author author)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ WebApiServer/Repositories/IAuthorsRepository.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using Common;
+
+namespace WebApiServer.Repositories
+{
+    public interface IAuthorsRepository
+    {
+        IEnumerable<Author> Get();
+        Author Get(int id);
+        int Create(Author author);
+        Author Update(Author author);
+        int Delete(int id);
+    }
+}
